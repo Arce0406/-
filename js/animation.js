863c025d7bd2215212378f9a3e7d9c46e664a566
FeatherArce <ice810406@gmail.com>
2022-10-26 05:41:37 +0800

    add copy animation

---
+++ js/animation.js
@@ -0,0 +1,29 @@
+const keyframe1 = [
+  { bottom: "-3em", opacity: 0 },
+  { bottom: "1em", opacity: 1 },
+];
+const keyframe2 = [
+  { bottom: "1em", opacity: 1 },
+  { bottom: "-2em", opacity: 0 },
+];
+const option1 = {
+  // timing options
+  duration: 200,
+  fill: "forwards",
+};
+
+function showCopied() {
+  let n = document.getElementById("notifiy");
+  n.style.display = "block";
+  //   let r = await n.animate(keyframe1, option1).finished;
+  let a1 = n.animate(keyframe1, option1);
+  a1.onfinish = (event) => {
+    setTimeout(function () {
+      if (n.style.display === "none") return;
+      let a2 = n.animate(keyframe2, option1);
+      a2.onfinish = (event) => {
+        n.style.display = "none";
+      };
+    }, 5000);
+  };
+}
--- js/main2.js
+++ js/main2.js
@@ -1,27 +1,14 @@
 document.addEventListener("DOMContentLoaded", () => {
   let tb_cc_script = document.getElementById("tb_cc_script");
   let btn_cc_script = document.getElementById("btn_cc_script");
-  let n = document.getElementById("notifiy");
-
+  
   //   console.log(tb_cc_script.value);
   btn_cc_script.addEventListener("click", () => {
     navigator.clipboard
       .writeText(tb_cc_script.value)
       .then(() => {
         // console.log("success");
-        n.style.display = "block";
-        n.animate(
-          [
-            // keyframes
-            { bottom: "-2em",  opacity: 0 },
-            { bottom: "1em",  opacity: 1 },
-          ],
-          {
-            // timing options
-            duration: 180,
-            fill: "forwards"
-          }
-        );
+        showCopied();
       })
       .catch((e) => alert("Copy failed.", e));
   });
